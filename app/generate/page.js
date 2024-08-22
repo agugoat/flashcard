cb6ea8bdd8d19f345f2ba7f5b4d4e45fc348c8ef
Prince Aguguo <paguguo8@gmail.com>
2024-08-21 20:20:11 -0400

    commit

--- app/generate/page.js
+++ app/generate/page.js
@@ -1,12 +1,24 @@
 'use client'
 
 import { useState, useEffect } from 'react'
-import { doc, collection, getDoc, writeBatch, getDocs } from 'firebase/firestore'
+import { doc, collection, getDoc, writeBatch, getDocs, } from 'firebase/firestore'
 import { useUser } from '@clerk/nextjs'
 import { useRouter, useSearchParams } from 'next/navigation'
 import { SignedIn, SignedOut, UserButton } from '@clerk/nextjs'
-import db from '../firebase';
 
+import { initializeApp } from 'firebase/app';
+import { getFirestore } from 'firebase/firestore';
+const firebaseConfig = {
+  apiKey: "AIzaSyAt0Cb9wDU4ZMQBpeKYfKaTGHeP9B_fbwE",
+  authDomain: "flashcards-b571a.firebaseapp.com",
+  projectId: "flashcards-b571a",
+  storageBucket: "flashcards-b571a.appspot.com",
+  messagingSenderId: "439220971734",
+  appId: "1:439220971734:web:7b7343038645e560274177",
+  measurementId: "G-XR33GDB2Z2"
+};
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
 
 export default function Generate() {
   const [text, setText] = useState('')
--- firebase.js
+++ firebase.js
@@ -1,11 +1,6 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+import { initializeApp } from 'firebase/app';
+import { getFirestore } from 'firebase/firestore';
 const firebaseConfig = {
   apiKey: "AIzaSyAt0Cb9wDU4ZMQBpeKYfKaTGHeP9B_fbwE",
   authDomain: "flashcards-b571a.firebaseapp.com",
@@ -15,12 +10,6 @@ const firebaseConfig = {
   appId: "1:439220971734:web:7b7343038645e560274177",
   measurementId: "G-XR33GDB2Z2"
 };
-
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
-
-
-
-
 export default db;
